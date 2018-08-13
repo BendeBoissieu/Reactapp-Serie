51dab10771cfb3ee9e3d9b436eca92c916ee9ad7
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-08-13 16:03:45 +0200

    new component liste

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class List extends Component {
+  render() {
+    return (
+      <div className="liste">Ici c'est ma liste</div>
+      )
+
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Here is my new app</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          By Benjamin de Boissieu
+          <List />
         </p>
+
       </div>
     );
   }
